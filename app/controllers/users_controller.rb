757b1224ea20b40741712ad207b71227bf1af80f
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-07 22:55:41 +0000

    add slug methods and validate presence of username, email, and password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,23 @@ class UsersController < ApplicationController
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/signup' do
+  end
 
+  post '/signup' do
+  end
 
+  get '/login' do
+  end
 
+  post '/login' do
+  end
 
+  get '/logout' do
 
+  end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  get '/users/:slug'
+  
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
   has_many :accounts
+  has_secure_password
+  validates_presence_of :username,:email, :password_digest
+
+  def slug
+    self.username.downcase.split('').join('-')
+  end
+
+  def self.find_by_slug(slug)
+    @all_user_info = []
+    self.all.each do |user|
+      if user.slug == slug
+        @all_user_info << user
+      end
+    end
+    @all_user_info[0]
+  end
 end
