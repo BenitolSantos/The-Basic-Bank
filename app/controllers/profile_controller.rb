93470103a715846023d924ffd4cd3de191843c72
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-10 18:07:47 +0000

    fix if ActiveRecord::Base.connection.migration_context.needs_migration? to if ActiveRecord::Migrator.needs_migration?

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,5 @@
-class ProfileController < ApplicationController
-  #"account" changed to "profile" do to account not working with ApplicationController 
+class ProfilesController < ApplicationController
+  #"account" changed to "profile" do to account not working with ApplicationController
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
