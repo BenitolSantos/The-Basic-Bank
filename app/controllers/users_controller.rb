3df03aff5485a0dcd00b4f989fa2d4b0b19b9883
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-10 15:36:02 +0000

    add get and post login, logout, and users/slug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,32 +6,57 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :"users/create_user"
     else
-      #redirect to ('/accounts')
+      redirect to ('/accounts')
     end
   end
 
   post '/signup' do
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect to ('/signup')
+    else
+      @user = User.create(username :params[:username], params[:password], email: params[:email])
+      @user.save
+      session[:user_id] = @user.id
 
+      redirect to ("/accounts")
+    end
   end
 
   get '/login' do
     if !logged_in?
       erb :"users/create_user"
     else
-      #redirect to ('/accounts')
+      redirect to ('/accounts')
     end
   end
 
   post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
 
+      redirect "/accounts"
+    else
+      redirect to '/signup'
+    end
   end
 
   get '/logout' do
-
-
+    if logged_in?
+      session.destroy
+      redirect to ('/login')
+    else
+      redirect to ('/')
+    end
   end
 
   get '/users/:slug' do
-
+    if logged_in?
+      @user = User.find_by_slug(params[:slug])
+      @accounts = @user.accounts
+      erb :"/users/show"
+    else
+      redirect to '/signup'
+    end
   end
 end
