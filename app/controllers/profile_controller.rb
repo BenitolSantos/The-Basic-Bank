a5d810d4d90df5387f566513d22508dc908b0a29
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-10 18:50:42 +0000

    create path routes for profilescontroller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,12 +3,26 @@ class ProfilesController < ApplicationController
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
+  get "/profiles" do
+  end
 
+  get "/profiles/new" do
+  end
 
+  get "/profiles/:id" do
+  end
 
+  get "/profiles/:id/edit" do
 
+  end
 
+  patch "/profiles/:id" do
 
+  end
+
+  delete "/profiles/:id" do
+
+  end
 
 
 
