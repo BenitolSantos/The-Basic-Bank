1da385af55850c11f83aac34d69277bddcf02450
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-10 02:24:29 +0000

    change exchanges_controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,4 @@
-class ExchangesController < ApplicationController
+class TransactionsController < ApplicationController
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
