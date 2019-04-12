35b6d58201adaa03dd2383bd0e33eb5bb3c0fa9b
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-12 04:57:53 +0000

    get transactions and get transactions/new

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,8 +2,22 @@ class TransactionsController < ApplicationController
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/transactions' do
+    if logged_in?
+      @tweets = Tweet.all
+      erb : ''
+    else
+      redirect to "/login"
+    end
+  end
 
-
+  get '/transactions/new' do
+    if logged_in?
+      erb '/transactions/new'
+    else
+      redirect to "/login"
+    end
+  end
 
 
 
