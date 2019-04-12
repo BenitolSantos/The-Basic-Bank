d17ce2cac1874c0424c9a18165ed99a1ad1be31f
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-12 01:53:48 +0000

    add balance and content to users and user_id to transactions

--- db/migrate/20190406191205_create_users.rb
+++ db/migrate/20190406191205_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email
+      t.string :content
+      t.integer :balance
       t.string :password_digest #
     end
   end
