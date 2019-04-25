bb39097bd52704ad610ffca0c535834aab470a5d
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-25 18:25:43 +0000

    sqlite3 doesn't accept floats so amount begins in cents

--- db/migrate/20190406191205_create_users.rb
+++ db/migrate/20190406191205_create_users.rb
@@ -1,13 +1,14 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration[4.2]
   def change
     create_table :users do |t|
       t.string :username
       t.string :email
       t.string :content
-      t.float :balance
+      t.integer :balance
       t.string :password_digest #
     end
   end
 end
 
 #rake db:drop to clear database
+#sqlite3 doesn't accept floats so amount begins in cents
--- db/migrate/20190410022658_create_transactions.rb
+++ db/migrate/20190410022658_create_transactions.rb
@@ -1,9 +1,11 @@
 class CreateTransactions < ActiveRecord::Migration[4.2]
   def change
     create_table :transactions do |t|
-      t.float :amount
+      t.integer :amount
       t.string :version
       t.integer :user_id
     end
   end
 end
+
+#sqlite3 doesn't accept floats so amount begins in cents
